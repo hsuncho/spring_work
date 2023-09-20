369278580b22c77cd10b18f5ad1e3bb18fb90514
hyeonseon <hsuncho@naver.com>
2023-09-20 17:41:13 +0900

    20230920 3 Tier Architecture 완료(DB 사용전)

--- SpringWebBasic/src/main/java/com/spring/basic/score/entity/Score.java
+++ SpringWebBasic/src/main/java/com/spring/basic/score/entity/Score.java
@@ -31,10 +31,12 @@ public class Score {
 	private double average;
 	private Grade grade;
 	
+	//객체 생성할 때 DTO를 보내면 Name을 꺼내서 멤버 변수에 DTO에 있는 사용자가 입력한 name을 입력할게
+	//changeScore에서는 dto를 전달받아서 초기화한 후 calcTotalAndAvg calcGrade 메서드를 부름
 	public Score(ScoreRequestDTO dto) {
 		this.stuName = dto.getName();
 		changeScore(dto);
-	}
+	} 
 	
 	 public void changeScore(ScoreRequestDTO dto) {
 	        this.kor = dto.getKor();
@@ -46,7 +48,7 @@ public class Score {
 
     private void calcGrade() {
         if (average >= 90) {
-            this.grade = Grade.A;
+            this.grade = Grade.A; //grade라는 변수에 enum에 선언된 상수 중 하나의 값을 매겨주고 있음
         } else if (average >= 80) {
             this.grade = Grade.B;
         } else if (average >= 70) {
@@ -59,7 +61,7 @@ public class Score {
     }
 
     private void calcTotalAndAvg() {
-        this.total = kor + eng + math;
+        this.total = this.kor + eng + math;
         this.average = total / 3.0;
     }
 	
